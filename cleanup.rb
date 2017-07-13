dfb08bef7438dab59610f0c83631ee205ff1d3c1
Artyom Bilun <artyom_bilun@epam.com>
2017-07-13 20:38:09 +0300

    Fixed processed files counting

--- cleanup.rb
+++ cleanup.rb
@@ -44,37 +44,40 @@ processed_files = 0
 
 Dir.chdir cli.config[:path]
 
+# Iterate over all .properties files
 Dir.glob('content/**/*.properties') do |filename|
   File.open(filename, 'r+') do |file|
     repo_name, creation_time, blob_name = ''
     is_deleted = false
 
     file.each_line do |line|
+      is_deleted = true if line =~ /deleted=true/
       repo_name = get_value(line) if line =~ /repo-name/
       creation_time = get_value(line).to_i / 1000 if line =~ /creationTime/
       blob_name = get_value(line) if line =~ /blob-name/
-      is_deleted = true if line =~ /deleted=true/
     end
 
     next if is_deleted
 
+    # If file is matching path and days conditions, then mark it as deleted
     if data_json.key?(repo_name)
       data_json[repo_name].each do |rule|
-        # check if path section exists
         next unless blob_name =~ /#{rule['path']}/
         days_before_today = sub_days(creation_time)
-        file.puts('deleted=true') if days_before_today > rule['days'].to_i
-        processed_files += 1
+        if days_before_today > rule['days'].to_i
+          processed_files += 1
+          file.puts('deleted=true')
+        end
       end
     end
   end
 end
 
-puts processed_files
+puts "Processed files: #{processed_files}"
 
 if processed_files > 0
-  File.open("#{cli.config[:path]}/metadata.properties", 'r+') do |file|
+  File.open("#{cli.config[:path]}/metadata.properties", 'a+') do |file|
     index_line = 'rebuildDeletedBlobIndex=true'
-    file.print("\n#{index_line}") unless file.grep(/#{index_line}/).any?
+    file.print(index_line) unless file.grep(/#{index_line}/).any?
   end
 end
